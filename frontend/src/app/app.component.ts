2c026056e46ba5be52100a2e8460bfd7890f0734
pixquilly <pixquilly@gmail.com>
2025-03-09 01:17:06 +0200

    created a post function.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
+
+
 import { HomeComponent } from './pages/home/home.component';
 import { CharactersComponent } from './pages/characters/characters.component';
 import { DialogueComponent } from './pages/dialogue/dialogue.component';
@@ -8,10 +10,12 @@ import { EventsComponent } from './pages/events/events.component';
 import { ChaptersComponent } from './pages/chapters/chapters.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomeComponent, CharactersComponent, DialogueComponent, QuestsComponent, EventsComponent, ChaptersComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HomeComponent, CharactersComponent, DialogueComponent, QuestsComponent, EventsComponent, ChaptersComponent, HeaderComponent, FooterComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- frontend/src/app/pages/characters/characters.component.ts
+++ frontend/src/app/pages/characters/characters.component.ts
@@ -1,11 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Form } from '@angular/forms';
 
 @Component({
   selector: 'app-characters',
-  imports: [],
   templateUrl: './characters.component.html',
-  styleUrl: './characters.component.scss'
+  styleUrl: './characters.component.scss',
+  imports: [CommonModule]
 })
 export class CharactersComponent {
+  data: any = null;
+  constructor(private http: HttpClient) {}
+  sendFormData(form: any) {
+    let body = {
+      fname: form.fname
+    };
+    this.http.post('https://jsonplaceholder.typicode.com/todos/1', body).subscribe((response) => {
+      this.data = JSON.stringify(response);
+      alert(this.data);
+    });
+    alert("success")
+  }
 
 }
